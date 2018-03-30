5139476f418c7dd6372351323b8121faf340019e
Shivam Prasad <prasadshivam2296@gmail.com>
2018-03-30 12:51:42 +0530

    Optimized the code size.

--- fast_Fibo.cpp
+++ fast_Fibo.cpp
@@ -4,67 +4,30 @@
 
 using namespace std;
 
-#define ll long long
-#define ii int,int
-#define MOD 1000000007
-#define PI 3.1415926535897932384626433832795028841971693993751
-#define all(arr)  arr.begin(),arr.end()
-#define FIND(arr,v) arr.find(v)!=arr.end()
-#define FOR(i,arr,n) for(i=arr;i<n;i++)
-#define nl cout<<"\n"
-#define spc cout<<" "
-#define check1 cout<<"#"
-#define check2 cout<<"*"
-#define reverse_pq priority_queue<int,vector<int>,greater<int> >
-#define VECTOR vector<int>
-#define SET set<int>
-#define do_it_fast ios_base::sync_with_stdio (false);cin.tie(NULL)
+long long MAT[2][2]={1,1,1,0};
 
-/** REMEMBER
-1. next_permutarrtion(all(V1));
-2. search(arrll(V1),all(V2))!=V1.end()
-3. S1.find(S2)
-4. lower_bound(all(V1),val);
-5. find(all(V1),val);
-6. copy(all(V1),dest_itr);
-*/
-ll MAT[2][2]={1,1,1,0};
-
-void fibo(ll n)
+void fibo(long long n)
 {
     if(n==0||n==1){
         return;
     }
-    if(n&1){
         fibo(n/2);
 
-        ll a=MAT[0][0]*MAT[0][0]+MAT[0][1]*MAT[1][0];
-        ll b=MAT[0][0]*MAT[0][1]+MAT[0][1]*MAT[1][1];
-        ll c=MAT[1][0]*MAT[0][0]+MAT[1][1]*MAT[1][0];
-        ll d=MAT[1][0]*MAT[0][1]+MAT[1][1]*MAT[1][1];
+        long long a=MAT[0][0]*MAT[0][0]+MAT[0][1]*MAT[1][0];
+        long long b=MAT[0][0]*MAT[0][1]+MAT[0][1]*MAT[1][1];
+        long long c=MAT[1][0]*MAT[0][0]+MAT[1][1]*MAT[1][0];
+        long long d=MAT[1][0]*MAT[0][1]+MAT[1][1]*MAT[1][1];
 
         MAT[0][0]=a;
         MAT[0][1]=b;
         MAT[1][0]=c;
         MAT[1][1]=d;
 
-        a=MAT[0][0]*1+MAT[0][1]*1;
-        b=MAT[0][0]*1+MAT[0][1]*0;
-        c=MAT[1][0]*1+MAT[1][1]*1;
-        d=MAT[1][0]*1+MAT[1][1]*0;
-
-        MAT[0][0]=a;
-        MAT[0][1]=b;
-        MAT[1][0]=c;
-        MAT[1][1]=d;
-    }
-    else{
-        fibo(n/2);
-
-        ll a=MAT[0][0]*MAT[0][0]+MAT[0][1]*MAT[1][0];
-        ll b=MAT[0][0]*MAT[0][1]+MAT[0][1]*MAT[1][1];
-        ll c=MAT[1][0]*MAT[0][0]+MAT[1][1]*MAT[1][0];
-        ll d=MAT[1][0]*MAT[0][1]+MAT[1][1]*MAT[1][1];
+   if(n&1){
+        long long a=MAT[0][0]*1+MAT[0][1]*1;
+        long long b=MAT[0][0]*1+MAT[0][1]*0;
+        long long c=MAT[1][0]*1+MAT[1][1]*1;
+        long long d=MAT[1][0]*1+MAT[1][1]*0;
 
         MAT[0][0]=a;
         MAT[0][1]=b;
